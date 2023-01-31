80e11ec865d23b9f3c285a737c78993a1ba67faa
dadiazr07 <dadiazr.07@gmail.com>
2023-01-31 00:36:06 +0000

    Condicional ternario

--- src/js/component/Counter.jsx
+++ src/js/component/Counter.jsx
@@ -4,10 +4,11 @@ import React from "react";
 export const Counter = (props) => {
     console.log(props.seconds)
     return (
-
+    
             <div className="counter bg-dark m-1 rounded-5">
-                {props.seconds}
+                {props.seconds < 10 ? props.seconds : '0'}
             </div>
+
 
     )
 }
